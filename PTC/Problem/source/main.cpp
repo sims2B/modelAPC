c9871c16dc3a3c0ed95155f15dec1078822c5bc2
Arnaud Malapert <arnaud.malapert@unice.fr>
2019-03-25 16:04:43 +0100

    remove deprecated projects

--- PTC/Problem/source/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Problem.h"
-
-int main( int ,char* argv[]){
-  std::ofstream output("inst_"+ (std::string)argv[1] + "_1_" + (std::string)argv[2] + "_10_"  + (std::string)argv[2]+"_Bthr.txt" , std::ios::out);
-  Problem P = generateProblem(atoi(argv[1]),1,atoi(argv[2]),10,atoi(argv[2]));
-  problem.writeInFile(output);
-  return 0;
-}
