c1b24e0972042c926f98a3a4561d2b946a3326f1
Arnaud Malapert <arnaud.malapert@unice.fr>
2020-03-10 22:36:16 +0100

    filter the cardinality variables #6

--- src/ilcConstraintAPC.cpp
+++ src/ilcConstraintAPC.cpp
@@ -33,18 +33,24 @@ void IlcRelax1SFConstraintI::initSequence()
 
 void IlcRelax1SFConstraintI::reduceCardFamily(int i)
 {
-  const int min = _x[i - 1].getMin();
-  int max = _x[i - 1].getMax();
-  const int flow = _f.getMax();
-  // std::cout << "FLOW " << flow << std::endl;
+  IlcIntVar x = _x[i - 1];
+  if(x.isFixed()) return;
+  const int min = x.getMin();
+  int max = x.getMax();
+  const int flowUB = _f.getMax();
   while (max > min)
   {
-    // std::cout << "MAX " << max << std::endl;
     s.setRequired(i, max);
     s.sequencing();
-    if (s.searching(flow))
+    if (s.searching(flowUB))
     {
-      _x[i - 1].setMax(max);
+#ifdef DEBUG
+      if (max < x.getMax())
+      {
+        std::cout << "FAMILY " << i << " CARD " << x.getMax() << "->" << max << std::endl;
+      }
+#endif
+      x.setMax(max);
       break;
     }
     max--;
@@ -78,7 +84,8 @@ bool IlcRelax1SFConstraintI::extendSequence(int size)
     else
       std::cout << "ADD FAMILY " << _n + orderSPT[f] << std::endl;
 #endif
-    if (opt > missing) {
+    if (opt > missing)
+    {
       opt = missing;
     }
     se.setRequired(_n + orderSPT[f], opt);
@@ -92,31 +99,33 @@ void IlcRelax1SFConstraintI::reduceCardMachine()
 {
   int f = _n - 1;
   const int flowUB = _f.getMax();
-   se.sequencing();
-   //std::cout << "COMPUTED LB " << se.searching() << " UB "<< flowUB << std::endl;
+  se.sequencing();
+  //std::cout << "COMPUTED LB " << se.searching() << " UB "<< flowUB << std::endl;
 
   while (!se.searching(flowUB))
   {
     while (f >= 0)
     {
-      int i = _n + orderSPT[f];
-    
-      if (se.getRequired(i) > 0) {
+      const int i = _n + orderSPT[f];
+      if (se.getRequired(i) > 0)
+      {
         se.decrementRequired(i);
         break;
       }
-        f--;
+      f--;
     }
-    if (f >= 0) {
+    if (f >= 0)
+    {
       se.sequencing();
-     #ifdef DEBUG
+#ifdef DEBUG
       se.printSequence();
       printf("\n");
-      #endif
+#endif
     }
     else
       break;
   }
+  _c.setMax(se.getSize());
 }
 
 void IlcRelax1SFConstraintI::propagate()
@@ -133,53 +142,24 @@ void IlcRelax1SFConstraintI::propagate()
   {
     for (int i = 1; i <= _n; i++)
     {
-      // const int oldMax = _x[i-1].getMax();
-      if (!_x[i - 1].isFixed())
         reduceCardFamily(i);
-      //  const int newMax = _x[i-1].getMax();
-      //   if(newMax != oldMax) {
-      //    std::cout << "N_F" <<  i << ": " << oldMax << " -> " << newMax <<
-      //    std::endl;
-      //  }
     }
   }
   if (propagationMask & 4)
   {
     // TODO trigger only if the card variable is not fixed ?
     initExtendedSequence();
-   
-    ///////////
-    // Temporary code while waiting for the card variable
-    int maxCard = 0;
-    for (int i = 1; i <= _n; i++)
+    if (extendSequence(_c.getMax()))
     {
-      maxCard += _x[i - 1].getMax();
-    }
-#ifdef DEBUG
- std::cerr << std::endl
-            << std::endl
-            << "START" << std::endl;
-    se.printSequence();
-    std::cout << "MAX CARD " << maxCard << " FLOWTIME " << _f.getMax() << std::endl;
-#endif
-    ///////////
-    if (extendSequence(maxCard))
-    {
- #ifdef DEBUG
-    se.printSequence();
-#endif
-reduceCardMachine();
-#ifdef DEBUG
-      std::cout << "NEW MAX CARD " << se.getSize() << std::endl;
-#endif
- //if(maxCard > se.getSize()) 
- std::cout << "UPDATE MAX CARD " << maxCard << " -> "<< se.getSize() << " REAL MAX CARD " << _c.getMax()<< std::endl;
- //if(se.getSize() == 0) exit(EXIT_SUCCESS);
+      reduceCardMachine();
     }
     else
     {
-      std::cerr << "Not enough optional jobs for the machine" << std::endl;
+      fail();
     }
+
+    std::cout << "UPDATE MAX CARD " << se.getSize() << " REAL MAX CARD " << _c.getMax() << std::endl;
+    //if(se.getSize() == 0) exit(EXIT_SUCCESS);
   }
 }
 
